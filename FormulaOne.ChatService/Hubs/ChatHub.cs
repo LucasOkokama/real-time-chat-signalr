e10ce0a755765d8f3cea15439da5dada1c3734e7
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-03-02 18:13:59 -0300

    feat: initial files

---
+++ FormulaOne.ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+
+public class ChatHub : Hub
+{
+	public async Task JoinChat(UserConnection conn)
+	{
+		await Clients.All.SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined!");
+	}
+
+	public async Task JoinSpecificChatRoom(UserConnection conn)
+	{
+		await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+		await Clients.Groups(conn.ChatRoom).SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined {conn.ChatRoom}!");
+    }
+}
