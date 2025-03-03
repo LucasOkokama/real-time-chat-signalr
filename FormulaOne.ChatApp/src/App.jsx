9f17e1b73a9044a00cd4c2381e2ae8db73c62523
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-03-02 21:16:35 -0300

    feat: create menu page

--- FormulaOne.ChatApp/src/App.jsx
+++ FormulaOne.ChatApp/src/App.jsx
@@ -1,17 +1,19 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import WaitingRoom from "./components/waitingroom";
 
 function App() {
   return (
     <div>
       <main>
         <Container>
-          <Row className="px-5 my-5">
-            <Col sm='12'>
-              <h1 className="font-weight-light"> Welcome to the F1 ChatApp </h1>
+          <Row className="my-5">
+            <Col sm='12' style={{textAlign: "center"}}>
+              <h1 className="font-weight-light"> Welcome to the Real Time Chat app </h1>
             </Col>
           </Row>
+          <WaitingRoom />
         </Container>
       </main>
     </div>
--- FormulaOne.ChatApp/src/components/waitingroom.jsx
+++ FormulaOne.ChatApp/src/components/waitingroom.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 const WaitingRoom = ({ joinChatRoom }) => {
+  const [username, setUsername] = useState();
+  const [chatroom, setChatroom] = useState();
 
+  return (
+    <Form
+      className="d-flex"
+      style={{ alignItems: "center", justifyContent: "center" }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        joinChatRoom(username, chatroom);
+      }}
+    >
+      <Row style={{ width: "650px" }}>
+        <Col sm={12}>
+          <Form.Group className="d-flex" style={{ gap: 16 }}>
+            <Form.Control
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Form.Control
+              placeholder="Chatroom"
+              onChange={(e) => setChatroom(e.target.value)}
+            />
+          </Form.Group>
+        </Col>
+
+        <Col sm={12}>
+          <hr />
+          <Button variant="success" type="submit" style={{width: "25%"}}>
+            Join
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
 };
 
-
-export default WaitingRoom
+export default WaitingRoom;
